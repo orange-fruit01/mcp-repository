5c9fb4277c7fcf88191c776f7931c812351e2b4a
orange-fruit01 <65435328+orange-fruit01@users.noreply.github.com>
2025-06-07 00:34:17 +0800

    Update mcp_main.py

--- server/mcp_main.py
+++ server/mcp_main.py
@@ -1,5 +1,18 @@
 from fastmcp import FastMCP
 from competitor_analysis_agent import graph
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
+
+# Allow all origins (not recommended for production)
+middleware = [
+    Middleware(
+        CORSMiddleware,
+        allow_origins=["*"],  # Allow all origins
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+]
 
 mcp = FastMCP(name="MyServer")
 
@@ -34,7 +47,8 @@ def competitor_analysis(company: str, industry: str, competitor: str) -> dict:
     return result
 
 if __name__ == "__main__":
-    mcp.run(transport="streamable-http",
-            host="0.0.0.0",
-            port=8000)
-            
+    import uvicorn
+
+    http_app = mcp.http_app(middleware=middleware)
+
+    uvicorn.run(http_app, host="0.0.0.0", port=8000)
